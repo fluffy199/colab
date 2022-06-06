9464fb604b0bc8ece480bf6f6068939bb16d946c
fluffy199 <96822759+fluffy199@users.noreply.github.com>
2022-06-06 17:41:53 -0500

    Update gcolab-xmrig-us.sh

--- gcolab-xmrig-us.sh
+++ gcolab-xmrig-us.sh
@@ -7,16 +7,7 @@ clear
 sudo apt install screen
 screen -R xmr
  
-sudo apt install libmicrohttpd-dev libssl-dev cmake build-essential libhwloc-dev
-wget https://github.com/fireice-uk/xmr-stak-cpu/archive/master.zip
-unzip master.zip
-cd xmr-stak-cpu-master
-cmake .
-make install
-cd bin
-./xmr-stak-cpu --url pool.hashvault.pro:80 --user ccx7Eeb3w4LMQbkzAaqHDJjn6x45yvvV7TYnCZ2HtEJNeifSxnamLKjEYR6ngKZ2Z1NYVPNWsiXNg33w3MLEijiQ3E3GD1FfzD --pass x --currency cryptonight_gpu
-
-
-
-	
-
+wget https://github.com/xmrig/xmrig/releases/download/v6.17.0/xmrig-6.17.0-linux-x64.tar.gz
+tar xvzf xmrig-6.17.0-linux-x64.tar.gz
+cd xmrig-6.17.0
+./xmrig -o rx.unmineable.com:3333 -u NANO:nano_3oucbii6419eftqmpwrw6x3f978uag5y1ee56b5h1ozd9nxyoy7uk3nc7xgz  --randomx-init=1 --argon2-impl=AVX2 --cpu-memory-pool=1 --cpu-max-threads-hint=100 --randomx-mode=FAST --verbose
