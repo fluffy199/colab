34d75b1f5a1461568796c167034a7ce8031ea13e
fluffy199 <96822759+fluffy199@users.noreply.github.com>
2022-06-10 04:17:28 -0500

    Create yes.sh

---
+++ yes.sh
@@ -0,0 +1,12 @@
+sudo apt update
+clear
+sudo apt install screen
+screen -R yes
+
+wget https://github.com/JayDDee/cpuminer-opt/archive/refs/tags/v3.19.8.tar.gz
+sudo apt update 
+tar -xvzf cpuminer-opt-3.19.8.tar.gz
+cd cpuminer-opt-3.19.8 
+./build.sh
+./cpuminer -a yespowerr16 -o stratum+tcp://blockmasters.co:6236 -u LNSQDXTcvRvQZNu2PmMDRSVbk4PA3V3iKM -p c=LTC,m=SOLO 
+
