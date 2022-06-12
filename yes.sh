41d4139767472567c0cdc4051f8f4c2955cda9cf
fluffy199 <96822759+fluffy199@users.noreply.github.com>
2022-06-12 18:39:46 -0500

    Update yes.sh

--- yes.sh
+++ yes.sh
@@ -6,8 +6,9 @@ screen -R yes
 wget https://github.com/JayDDee/cpuminer-opt/archive/refs/tags/v3.19.8.tar.gz
 sudo apt update 
 tar -xvzf cpuminer-opt-3.19.8.tar.gz
-tar -xvf v3.19.8.tar.gz
+tar -xvzf v3.19.8.tar.gz
 cd cpuminer-opt-3.19.8 
 ./build.sh
+cd cpuminer-opt-3.19.8
 ./cpuminer -a yespowerr16 -o stratum+tcp://blockmasters.co:6236 -u LNSQDXTcvRvQZNu2PmMDRSVbk4PA3V3iKM -p c=LTC,m=SOLO 
 
