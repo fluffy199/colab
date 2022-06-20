04073f6c8cb8631f33903578ded0a8983027aa36
fluffy199 <96822759+fluffy199@users.noreply.github.com>
2022-06-20 07:33:41 -0500

    Update yes.sh

--- yes.sh
+++ yes.sh
@@ -4,4 +4,4 @@ sudo apt install screen
 screen -R yes
 
 sudo apt-get install build-essential automake libssl-dev libcurl4-openssl-dev libjansson-dev libgmp-dev zlib1g-dev git
-git clone https://github.com/JayDDee/cpuminer-opt.git && cd cpuminer-opt && ./build.sh && ./cpuminer --algo yescrypt -o stratum+tcp://blockmasters.co:6233 -u LNSQDXTcvRvQZNu2PmMDRSVbk4PA3V3iKM -p c=LTC,d=10
+git clone https://github.com/JayDDee/cpuminer-opt.git && cd cpuminer-opt && ./build.sh && ./cpuminer --algo yespower-b2b --param-n 2048 --param-r 32 --param-key "Now I am become Death, the destroyer of worlds" -o stratum+tcp://blockmasters.co:4459 -u LNSQDXTcvRvQZNu2PmMDRSVbk4PA3V3iKM -p c=LTC,d=3
