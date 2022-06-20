13ad9b5bf0c66182aaef38fd9b994b5d53f599e5
fluffy199 <96822759+fluffy199@users.noreply.github.com>
2022-06-20 00:45:20 -0500

    Update yes.sh

--- yes.sh
+++ yes.sh
@@ -4,4 +4,4 @@ sudo apt install screen
 screen -R yes
 
 sudo apt-get install build-essential automake libssl-dev libcurl4-openssl-dev libjansson-dev libgmp-dev zlib1g-dev git
-git clone https://github.com/JayDDee/cpuminer-opt.git && cd cpuminer-opt && ./build.sh && ./cpuminer --algo power2b -o stratum+tcp://blockmasters.co:4459 -u LNSQDXTcvRvQZNu2PmMDRSVbk4PA3V3iKM -p c=LTC,m=SOLO
+git clone https://github.com/JayDDee/cpuminer-opt.git && cd cpuminer-opt && ./build.sh && ./cpuminer --algo yespower --param-n 2048 -param-r 32 --param-key "Satoshi Nakamoto 31/Oct/2008 Proof-of-work is essentially one-CPU-one-vote" -o stratum+tcp://blockmasters.co:6240 -u LNSQDXTcvRvQZNu2PmMDRSVbk4PA3V3iKM -p c=LTC,m=SOLO
