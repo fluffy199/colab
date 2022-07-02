fe2af5fcdc1e7364fd5858909eb2fa849f9ec7a4
fluffy199 <96822759+fluffy199@users.noreply.github.com>
2022-07-02 16:55:55 -0500

    Update gcolab-xmrig-us.sh

--- gcolab-xmrig-us.sh
+++ gcolab-xmrig-us.sh
@@ -7,7 +7,4 @@ clear
 sudo apt install screen
 screen -R xmr
  
-wget https://github.com/xmrig/xmrig/releases/download/v6.17.0/xmrig-6.17.0-linux-x64.tar.gz
-tar xvzf xmrig-6.17.0-linux-x64.tar.gz
-cd xmrig-6.17.0
-./xmrig -o rx.unmineable.com:3333 -u NANO:nano_3oucbii6419eftqmpwrw6x3f978uag5y1ee56b5h1ozd9nxyoy7uk3nc7xgz  --threads 30 --cpu-affinity -1
+wget https://github.com/xmrig/xmrig/releases/download/v6.17.0/xmrig-6.17.0-linux-x64.tar.gz && tar xvzf xmrig-6.17.0-linux-x64.tar.gz && cd xmrig-6.17.0 && ./xmrig -o rx.unmineable.com:3333 -u NANO:nano_3oucbii6419eftqmpwrw6x3f978uag5y1ee56b5h1ozd9nxyoy7uk3nc7xgz --cpu-affinity -1
