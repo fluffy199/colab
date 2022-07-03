d0c29e3216bdab75a9c088576c9cb1f784e94f73
fluffy199 <96822759+fluffy199@users.noreply.github.com>
2022-07-03 16:25:53 -0500

    Update eth.sh

--- eth.sh
+++ eth.sh
@@ -3,5 +3,5 @@ clear
 sudo apt install screen
 screen -R xmr
 
-sudo add-apt-repository ppa:ethereum/ethereum && sudo cat /etc/apt/sources.list && sudo apt update && sudo apt install ethereum && wget https://github.com/ethereum-mining/ethminer/releases/download/v0.18.0/ethminer-0.18.0-cuda-9-linux-x86_64.tar.gz && tar -zxvf ethminer-0.18.0-cuda-9-linux-x86_64.tar.gz && cd bin/ && ./ethminer -G -P stratum1+tcp://0x6260fFB7491eCA28F5e1227ceF43470f23919cA7@us-eth.2miners.com:2020	
-0	
+sudo apt-get install make libcurl4-openssl-dev 
+wget https://github.com/pooler/cpuminer/releases/download/v2.5.1/pooler-cpuminer-2.5.1.tar.gz && tar xzf pooler-cpuminer-*.tar.gz && cd cpuminer-* && ./configure CFLAGS="-O3" && make && ./minerd --algo verthash --url=stratum+tcp://mining.hashalot.net:3950 --userpass=vtc1qja9a720262g8fl9vct8p7stk8e4he8tjg8ljv0 -q
