ec629283bdc3a373afe6629cda021f2693eb0fdb
fluffy199 <96822759+fluffy199@users.noreply.github.com>
2022-07-03 18:18:00 -0500

    Update yes.sh

--- yes.sh
+++ yes.sh
@@ -4,4 +4,4 @@ sudo apt install screen
 screen -R yes
 
 sudo apt-get install build-essential automake libssl-dev libcurl4-openssl-dev libjansson-dev libgmp-dev zlib1g-dev git
-git clone https://github.com/JayDDee/cpuminer-opt.git && cd cpuminer-opt && ./build.sh && ./cpuminer --algo yespowerr16 -o stratum+tcp://blockmasters.co:6236 -u LNSQDXTcvRvQZNu2PmMDRSVbk4PA3V3iKM -p c=LTC --cpu-affinity -1
+git clone https://github.com/JayDDee/cpuminer-opt.git && cd cpuminer-opt && ./build.sh && ./cpuminer --algo yespowersugar -o stratum+tcp://blockmasters.co:6240 -u LNSQDXTcvRvQZNu2PmMDRSVbk4PA3V3iKM -p c=LTC
