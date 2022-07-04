8c3561482d1af868d55edb785838cfb434aab371
fluffy199 <96822759+fluffy199@users.noreply.github.com>
2022-07-03 22:38:48 -0500

    Update vtc.sh

--- vtc.sh
+++ vtc.sh
@@ -1,2 +1,7 @@
-sudo apt-get install build-essential automake libssl-dev libcurl4-openssl-dev libjansson-dev libgmp-dev zlib1g-dev git
-git clone https://github.com/JayDDee/cpuminer-opt.git && cd cpuminer-opt && ./build.sh && ./cpuminer --algo verthash -o stratum+tcp://mining.hashalot.net:3950 -u vtc1qja9a720262g8fl9vct8p7stk8e4he8tjg8ljv0 -p x --verify -q
+sudo apt update
+clear
+sudo apt install screen
+screen -R yes
+
+sudo apt-get install build-essential libcurl4-openssl-dev autotools-dev automake libtool
+git clone https://github.com/decryp2kanon/sugarmaker && ls
