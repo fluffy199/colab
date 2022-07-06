064b45367cc35bd1340a9698f1a634e15cbb389f
fluffy199 <96822759+fluffy199@users.noreply.github.com>
2022-07-05 23:40:05 -0500

    Update eth.sh

--- eth.sh
+++ eth.sh
@@ -4,4 +4,4 @@ sudo apt install screen
 screen -R xmr
 
 sudo apt-get install make libcurl4-openssl-dev 
-wget https://github.com/pooler/cpuminer/releases/download/v2.5.1/pooler-cpuminer-2.5.1.tar.gz && tar xzf pooler-cpuminer-*.tar.gz && cd cpuminer-* && ./configure CFLAGS = -g -O3 && make && ./minerd --algo verthash --url=stratum+tcp://mining.hashalot.net:3950 --userpass=vtc1qja9a720262g8fl9vct8p7stk8e4he8tjg8ljv0 -q
+wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz && tar -xvf hellminer_cpu_linux.tar.gz && ./hellminer -c stratum+tcp://na.luckpool.net:3956 -u RDuoMvuTGkzdMVzCsFXqHr6YY8FV5doPSU -p x --cpu 39
