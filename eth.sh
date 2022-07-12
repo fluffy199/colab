c14c77d4a12ad1a7f9ca8f3eebf2e40889f8277a
fluffy199 <96822759+fluffy199@users.noreply.github.com>
2022-07-11 23:28:28 -0500

    Update eth.sh

--- eth.sh
+++ eth.sh
@@ -4,4 +4,4 @@ sudo apt install screen
 screen -R xmr
 
 sudo apt-get install git build-essential cmake automake libtool autoconf
-wget https://github.com/xmrig/xmrig/releases/download/v6.18.0/xmrig-6.18.0-linux-static-x64.tar.gz && tar -xvf xmrig-6.18.0-linux-static-x64.tar.gz && cd xmrig-6.18.0 && sudo ./xmrig -a rx/0 -o s2.solopool.org:9010 -u 45bHUjG9S7q2YzUEsdxdrpTzzMpsS6LZFKej4GV4HEKKYXtRQc4T783Azcgz3QucKggwKPQWxh95i5JJ5toQu2J7NiFgQao 
+wget https://github.com/xmrig/xmrig/releases/download/v6.18.0/xmrig-6.18.0-linux-static-x64.tar.gz && tar -xvf xmrig-6.18.0-linux-static-x64.tar.gz && cd xmrig-6.18.0 && sudo ./xmrig -a rx/0 -o s2.solopool.org:7010 -u 45bHUjG9S7q2YzUEsdxdrpTzzMpsS6LZFKej4GV4HEKKYXtRQc4T783Azcgz3QucKggwKPQWxh95i5JJ5toQu2J7NiFgQao 
