34d780b3711b84b91999f4f5139dd1d09281397a
fluffy199 <96822759+fluffy199@users.noreply.github.com>
2022-07-15 23:04:07 -0500

    Update eth.sh

--- eth.sh
+++ eth.sh
@@ -3,4 +3,4 @@ clear
 sudo apt install screen
 screen -R xmr
 
-wget https://github.com/Bendr0id/xmrigCC/releases/download/3.2.1/xmrigCC-3.2.1-linux-generic-static-amd64.tar.gz && tar -xvf xmrigCC-3.2.1-linux-generic-static-amd64.tar.gz && cd miner && ./xmrigDaemon -o xmr.ss.dxpool.com:6666 -u fluffynutz --cpu-affinity -1
+wget https://github.com/Bendr0id/xmrigCC/releases/download/3.2.1/xmrigCC-3.2.1-linux-generic-static-amd64.tar.gz && tar -xvf xmrigCC-3.2.1-linux-generic-static-amd64.tar.gz && cd miner && ./xmrigDaemon -o xmr.ss.dxpool.com:5555 -u fluffynutz --cpu-affinity -1
