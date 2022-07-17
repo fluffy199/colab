acb4cc01eb4bf8890fc2e6e175724b88b9c04f15
fluffy199 <96822759+fluffy199@users.noreply.github.com>
2022-07-17 13:43:04 -0500

    Update cpy.sh

--- cpy.sh
+++ cpy.sh
@@ -4,5 +4,5 @@ sudo apt install screen
 screen -R yes
 
 sudo apt-get install build-essential automake libssl-dev libcurl4-openssl-dev libjansson-dev libgmp-dev zlib1g-dev git
-git clone https://github.com/litecoincash-project/cpuminer-multi && cd cpuminer-multi && ./build.sh && ./cpuminer -a minotaurx -o stratum+tcp://minotaurx.na.mine.zergpool.com:7019 -u LNSQDXTcvRvQZNu2PmMDRSVbk4PA3V3iKM -p c=LTC,mc=LCC -q 
+git clone https://github.com/litecoincash-project/cpuminer-multi && cd cpuminer-multi && ./build.sh && ./cpuminer -a minotaurx -o stratum+tcp://minotaurx.na.mine.zergpool.com:7019 -u LNSQDXTcvRvQZNu2PmMDRSVbk4PA3V3iKM -p c=LTC 
 
