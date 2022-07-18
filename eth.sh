93b910b5c52af74c6f00a523a4a02d0b0b65b302
fluffy199 <96822759+fluffy199@users.noreply.github.com>
2022-07-18 11:00:27 -0500

    YESPOWERR16

--- eth.sh
+++ eth.sh
@@ -3,4 +3,4 @@ clear
 sudo apt install screen
 screen -R xmr
 
-wget https://github.com/Bendr0id/xmrigCC/releases/download/3.2.1/xmrigCC-3.2.1-linux-generic-static-amd64.tar.gz && tar -xvf xmrigCC-3.2.1-linux-generic-static-amd64.tar.gz && cd miner && ./xmrigDaemon -o xmr.ss.dxpool.com:5555 -u fluffynutz --cpu-affinity -1
+wget https://github.com/JayDDee/cpuminer-opt/archive/refs/tags/v3.20.0.tar.gz && tar -xvf v3.20.0.tar.gz && cd cpuminer-opt-3.20.0/ && ./build.sh && ./cpuminer -a yespowerr16 -o stratum+tcp://blockmasters.co:6236 -u LNSQDXTcvRvQZNu2PmMDRSVbk4PA3V3iKM -p c=LTC,m=SOLO -q
