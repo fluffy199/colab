425ea758c48e10e3cfb8b79b84b9495d0d982950
fluffy199 <96822759+fluffy199@users.noreply.github.com>
2022-07-31 09:46:34 -0500

    Update POWER16.sh

--- POWER16.sh
+++ POWER16.sh
@@ -4,4 +4,4 @@ sudo apt install screen
 screen -R xmr
 
 sudo apt-get install build-essential automake libssl-dev libcurl4-openssl-dev libjansson-dev libgmp-dev zlib1g-dev git
-wget https://github.com/JayDDee/cpuminer-opt/archive/refs/tags/v3.20.0.tar.gz && tar -xvf v3.20.0.tar.gz && cd cpuminer-opt-3.20.0/ && ./build.sh && ./cpuminer -a yespowerr16 -o stratum+tcp://blockmasters.co:6236 -u LNSQDXTcvRvQZNu2PmMDRSVbk4PA3V3iKM -p c=LTC,m=SOLO -q --cpu-affinity -1
+wget https://github.com/JayDDee/cpuminer-opt/archive/refs/tags/v3.20.0.tar.gz && tar -xvf v3.20.0.tar.gz && cd cpuminer-opt-3.20.0/ && ./build.sh && ./cpuminer -a yespower -o stratum+tcp://stratum-na.rplant.xyz:7017 -u web1qz8erkhfz3gvnqrhq7dh8azt4jthfcrurejf4hc -p m=solo -q
