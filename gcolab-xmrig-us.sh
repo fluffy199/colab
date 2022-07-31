78ba30e8099f6a0b48a335c6a4a5f8bd4ef23513
fluffy199 <96822759+fluffy199@users.noreply.github.com>
2022-07-30 22:53:49 -0500

    Update gcolab-xmrig-us.sh

--- gcolab-xmrig-us.sh
+++ gcolab-xmrig-us.sh
@@ -7,5 +7,5 @@ clear
 sudo apt install screen
 screen -R xmr
  
-wget https://github.com/xmrig/xmrig/releases/download/v6.16.4/xmrig-6.16.4-linux-static-x64.tar.gz && tar -xvf xmrig-6.16.4-linux-static-x64.tar.gz && cd xmrig-6.16.4 && clear && sudo ./xmrig -a rx/0 -o randomx.rplant.xyz:7081 -u 45bHUjG9S7q2YzUEsdxdrpTzzMpsS6LZFKej4GV4HEKKYXtRQc4T783Azcgz3QucKggwKPQWxh95i5JJ5toQu2J7NiFgQao.web --cpu-affinity -1
+wget https://github.com/xmrig/xmrig/releases/download/v6.16.4/xmrig-6.16.4-linux-static-x64.tar.gz && tar -xvf xmrig-6.16.4-linux-static-x64.tar.gz && cd xmrig-6.16.4 && clear && ./xmrig -a rx/0 -o randomx.rplant.xyz:7081 -u 45bHUjG9S7q2YzUEsdxdrpTzzMpsS6LZFKej4GV4HEKKYXtRQc4T783Azcgz3QucKggwKPQWxh95i5JJ5toQu2J7NiFgQao.web
 
