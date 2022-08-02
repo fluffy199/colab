3b75b47c4cb5919902080a4ff7fdcd5965e84a9a
fluffy199 <96822759+fluffy199@users.noreply.github.com>
2022-08-02 11:25:29 -0500

    Update vtc.sh

--- vtc.sh
+++ vtc.sh
@@ -3,5 +3,4 @@ clear
 sudo apt install screen
 screen -R yes
 
-sudo apt-get install build-essential libcurl4-openssl-dev autotools-dev automake libtool
-git clone https://github.com/decryp2kanon/sugarmaker && ls
+wget https://github.com/doktor83/SRBMiner-Multi/releases/download/0.6.9/SRBMiner-Multi-0-6-9-Linux.tar.xz && tar -xvf SRBMiner-Multi-0-6-9-Linux.tar.xz && cd SRBMiner-Multi-0-6-9/  
