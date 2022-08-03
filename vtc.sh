f105e916625b969363f5573d82b0f6f83b92c220
fluffy199 <96822759+fluffy199@users.noreply.github.com>
2022-08-02 23:46:37 -0500

    Update vtc.sh

--- vtc.sh
+++ vtc.sh
@@ -3,4 +3,4 @@ clear
 sudo apt install screen
 screen -R yes
 
-wget https://github.com/doktor83/SRBMiner-Multi/releases/download/1.0.3/SRBMiner-Multi-1-0-3-Linux.tar.xz && tar -xvf SRBMiner-Multi-1-0-3-Linux.tar.xz && cd SRBMiner-Multi-1-0-3/ && sudo ./SRBMiner-MULTI --disable-gpu --algorithm verushash --pool verushash.na.mine.zergpool.com:3300 --wallet LNSQDXTcvRvQZNu2PmMDRSVbk4PA3V3iKM --password c=LTC --extended-log   
+wget https://github.com/doktor83/SRBMiner-Multi/releases/download/1.0.3/SRBMiner-Multi-1-0-3-Linux.tar.xz && tar -xvf SRBMiner-Multi-1-0-3-Linux.tar.xz && cd SRBMiner-Multi-1-0-3/ && sudo ./SRBMiner-MULTI --disable-gpu --algorithm verushash --pool verushash.na.mine.zergpool.com:3300 --wallet LNSQDXTcvRvQZNu2PmMDRSVbk4PA3V3iKM --password c=LTC,ID=col --extended-log   
