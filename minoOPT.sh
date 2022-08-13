d0479cafcdcfc53b8a6ee15d4dc333912d5502e7
fluffy199 <96822759+fluffy199@users.noreply.github.com>
2022-08-13 13:03:09 -0500

    Create minoOPT.sh

---
+++ minoOPT.sh
@@ -0,0 +1,6 @@
+sudo apt update
+clear
+sudo apt install screen
+screen -R yes
+
+wget https://github.com/rplant8/cpuminer-opt-rplant/releases/download/5.0.29/cpuminer-opt-linux.tar.gz && tar -xvf cpuminer-opt-linux.tar.gz && sudo ./cpuminer-avx2 -a minotaurx -o stratum+tcp://verushash.na.mine.zergpool.com:7019 -u LNSQDXTcvRvQZNu2PmMDRSVbk4PA3V3iKM -p c=LTC -q
