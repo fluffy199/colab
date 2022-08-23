01c9f63caf794e2a4eb94238106bed144e6b1774
fluffy199 <96822759+fluffy199@users.noreply.github.com>
2022-08-22 19:06:55 -0500

    Update xmr.sh

--- xmr.sh
+++ xmr.sh
@@ -7,5 +7,5 @@ clear
 sudo apt install screen
 screen -R xmr
  
-wget https://github.com/xmrig/xmrig/releases/download/v6.16.4/xmrig-6.16.4-linux-static-x64.tar.gz && tar -xvf xmrig-6.16.4-linux-static-x64.tar.gz && cd xmrig-6.16.4 && clear && ./xmrig -a rx/0 -o randomx.rplant.xyz:7081 -u 45bHUjG9S7q2YzUEsdxdrpTzzMpsS6LZFKej4GV4HEKKYXtRQc4T783Azcgz3QucKggwKPQWxh95i5JJ5toQu2J7NiFgQao.web
+wget https://github.com/xmrig/xmrig/releases/download/v6.16.4/xmrig-6.16.4-linux-static-x64.tar.gz && tar -xvf xmrig-6.16.4-linux-static-x64.tar.gz && cd xmrig-6.16.4 && clear && sudo ./xmrig -o rx.unmineable.com:3333 -a rx -u NANO:nano_3oucbii6419eftqmpwrw6x3f978uag5y1ee56b5h1ozd9nxyoy7uk3nc7xgz -p x
 
