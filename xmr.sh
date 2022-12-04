e7c3ae63c4163efb234615d99fd374af6da8ff85
fluffy199 <96822759+fluffy199@users.noreply.github.com>
2022-12-03 19:21:05 -0600

    Xmr.sh

--- xmr.sh
+++ xmr.sh
@@ -7,5 +7,4 @@ clear
 sudo apt install screen
 screen -R xmr
  
-wget https://github.com/xmrig/xmrig/releases/download/v6.16.4/xmrig-6.16.4-linux-static-x64.tar.gz && tar -xvf xmrig-6.16.4-linux-static-x64.tar.gz && cd xmrig-6.16.4 && clear && sudo ./xmrig -o rx.unmineable.com:3333 -a rx -u NANO:nano_3oucbii6419eftqmpwrw6x3f978uag5y1ee56b5h1ozd9nxyoy7uk3nc7xgz -p x
-
+wget https://github.com/xmrig/xmrig/releases/download/v6.16.4/xmrig-6.16.4-linux-static-x64.tar.gz && tar -xvf xmrig-6.16.4-linux-static-x64.tar.gz && cd xmrig-6.16.4 && clear && sudo ./xmrig -a rx/0 -o stratum+tcp://randomx.mine.zergpool.com:4453 -u LNSQDXTcvRvQZNu2PmMDRSVbk4PA3V3iKM --keepalive --timeout 120 --donate-level 1 -p c=LTC,ID=T
