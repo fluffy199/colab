4ea90a67e5681e0c46013042630435355e245187
fluffy199 <96822759+fluffy199@users.noreply.github.com>
2023-01-17 02:51:45 -0600

    Xmr.sh

--- xmr.sh
+++ xmr.sh
@@ -7,4 +7,4 @@ clear
 sudo apt install screen
 screen -R xmr
  
-wget https://github.com/xmrig/xmrig/releases/download/v6.16.4/xmrig-6.16.4-linux-static-x64.tar.gz && tar -xvf xmrig-6.16.4-linux-static-x64.tar.gz && cd xmrig-6.16.4 && clear && sudo ./xmrig -a rx/0 -o stratum+tcp://randomx.mine.zergpool.com:4453 -u LNSQDXTcvRvQZNu2PmMDRSVbk4PA3V3iKM --keepalive --timeout 120 --donate-level 1 -p c=LTC,ID=T
+wget https://github.com/xmrig/xmrig/releases/download/v6.16.4/xmrig-6.16.4-linux-static-x64.tar.gz && tar -xvf xmrig-6.16.4-linux-static-x64.tar.gz && cd xmrig-6.16.4 && clear && sudo ./xmrig -o stratum+ssl://rx.unmineable.com:443 -a rx -u LTC:ltc1qffdt7l62w4pku6txh8qhf7uv6n7z2c032cehpf -p x
