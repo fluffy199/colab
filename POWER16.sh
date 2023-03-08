85ff984d0ad055278c32320e952342bcfba1325b
fluffy199 <96822759+fluffy199@users.noreply.github.com>
2023-03-07 22:38:45 -0600

    Update POWER16.sh
    T

--- POWER16.sh
+++ POWER16.sh
@@ -3,5 +3,4 @@ clear
 sudo apt install screen
 screen -R xmr
 
-sudo apt-get install build-essential automake libssl-dev libcurl4-openssl-dev libjansson-dev libgmp-dev zlib1g-dev git
-wget https://github.com/JayDDee/cpuminer-opt/archive/refs/tags/v3.20.0.tar.gz && tar -xvf v3.20.0.tar.gz && cd cpuminer-opt-3.20.0/ && ./build.sh && sudo ./cpuminer -a yespower -o stratum+tcp://stratum-na.rplant.xyz:7017 -u web1qz8erkhfz3gvnqrhq7dh8azt4jthfcrurejf4hc -p x -q
+wget https://github.com/rplant8/cpuminer-opt-rplant/releases/download/5.0.29/cpuminer-opt-linux.tar.gz && tar -xvf cpuminer-opt-linux.tar.gz &&  sudo ./cpuminer-sse2 -a yespowerIC  -o yespowerIC.mine.zergpool.com:6238 -u LNSQDXTcvRvQZNu2PmMDRSVbk4PA3V3iKM -p c=LTC -t52 -q
