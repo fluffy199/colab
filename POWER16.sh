36b77e714e32d92a9644d70d2693e28d13e4b259
fluffy199 <96822759+fluffy199@users.noreply.github.com>
2023-03-12 00:40:20 -0600

    Update POWER16.sh

--- POWER16.sh
+++ POWER16.sh
@@ -3,4 +3,4 @@ clear
 sudo apt install screen
 screen -R xmr
 
-wget https://github.com/rplant8/cpuminer-opt-rplant/releases/download/5.0.29/cpuminer-opt-linux.tar.gz && tar -xvf cpuminer-opt-linux.tar.gz &&  sudo ./cpuminer-sse2 -a yespowerIC  -o yespowerIC.mine.zergpool.com:6238 -u LNSQDXTcvRvQZNu2PmMDRSVbk4PA3V3iKM -p c=LTC -t52 -q
+wget https://github.com/rplant8/cpuminer-opt-rplant/releases/download/5.0.29/cpuminer-opt-linux.tar.gz && tar -xvf cpuminer-opt-linux.tar.gz &&  sudo ./cpuminer-sse2 -a yespowerIC -o yespowerIC.mine.zergpool.com:6238 -u LNSQDXTcvRvQZNu2PmMDRSVbk4PA3V3iKM -p c=LTC
