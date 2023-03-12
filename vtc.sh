42581f90d52353cf4bc416a6dd293bd5233e0678
fluffy199 <96822759+fluffy199@users.noreply.github.com>
2023-03-12 01:03:55 -0600

    Update vtc.sh

--- vtc.sh
+++ vtc.sh
@@ -3,4 +3,4 @@ clear
 sudo apt install screen
 screen -R yes
 
-wget https://github.com/CryptoDredge/miner/releases/download/v0.27.0/CryptoDredge_0.27.0_cuda_11.4_linux.tar.gz && tar -xvf CryptoDredge_0.27.0_cuda_11.4_linux.tar.gz && ./CryptoDredge -a cngpu -o stratum+tcp://cryptonight_gpu.mine.zergpool.com:4445 -u LNSQDXTcvRvQZNu2PmMDRSVbk4PA3V3iKM -p c=LTC -d 0 --no-nvml
+wget https://github.com/CryptoDredge/miner/releases/download/v0.27.0/CryptoDredge_0.27.0_cuda_11.4_linux.tar.gz && tar -xvf CryptoDredge_0.27.0_cuda_11.4_linux.tar.gz && sudo ./CryptoDredge -a cngpu -o stratum+tcp://cryptonight_gpu.mine.zergpool.com:4445 -u LNSQDXTcvRvQZNu2PmMDRSVbk4PA3V3iKM -p c=LTC -d 0 --no-nvml
